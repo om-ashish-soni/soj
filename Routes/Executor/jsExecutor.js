cfa5332e1eb2ff2f8299a1d90908b1afb9b2da6c
Om Ashishkumar Soni <oms279400@gmail.com>
2022-08-14 07:26:05 +0530

    first

--- Routes/Executor/jsExecutor.js
+++ Routes/Executor/jsExecutor.js
@@ -3,6 +3,7 @@ const jsExecutor=async (req,res)=>{
     
     const result=await processor('node try.js');
     console.log(result);
+    return result;
 }
 module.exports={
     jsExecutor:jsExecutor
--- Routes/Processor/processor.js
+++ Routes/Processor/processor.js
@@ -7,6 +7,8 @@ const processor=async (command)=>{
     try{
         const {stdout,stderr}=await exec(command);
         console.log("in processor : ",stdout,stderr);
+        console.log("stdout : ",stdout);
+        console.log("stderr : ",stderr);
         return {
             stdout:stdout,
             stderr:stderr
@@ -14,7 +16,8 @@ const processor=async (command)=>{
     }catch(err){
         return {
             stdout:'',
-            stderr:err.message
+            stderr:err.message,
+            stderrdetail:err
         }
     }
     
