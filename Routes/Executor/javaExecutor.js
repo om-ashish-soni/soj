3d459ffbfaeba372409d72b11acd19a971880f81
Om Ashishkumar Soni <oms279400@gmail.com>
2022-08-14 13:39:40 +0530

    shree ganeshay namah , ssv no jay jay kar ho

--- Routes/Executor/javaExecutor.js
+++ Routes/Executor/javaExecutor.js
@@ -3,7 +3,7 @@ const fs=require('fs');
 const path=require('path')
 const {processor} = require('../Processor/processor')
 
-const javaExecutor=async (dirpath,lang,code,input)=>{
+const javaExecutorLocally=async (dirpath,lang,code,input)=>{
 
     // console.log("java : ",java);
 
@@ -40,6 +40,24 @@ const javaExecutor=async (dirpath,lang,code,input)=>{
     }
 }
 
+const javaExecutor=async (dirpath,lang,code,input)=>{
+
+    console.log("java : ",java);
+
+    // let filename='Main.java';
+    // let filepath=path.join(dirpath,filename);
+    // fs.writeFileSync(filepath,code);
+
+    // let inputFileName='input_java.txt';
+    // let inputFilePath=path.join(dirpath,inputFileName);
+    // fs.writeFileSync(inputFilePath,input);
+
+    return {
+        "java":java.hasOwnProperty("runFile")
+    }
+}
 module.exports={
-    javaExecutor:javaExecutor
+    javaExecutor:javaExecutor,
+    javaExecutorLocally:javaExecutorLocally
+
 }
