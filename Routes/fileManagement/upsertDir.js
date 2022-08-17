8fe66b15e2003cbf07bd622961f5030a530ff697
Om Ashishkumar Soni <oms279400@gmail.com>
2022-08-17 12:05:24 +0530

    shree ganeshay namah , ssv no jay jay kar ho

--- Routes/fileManagement/upsertDir.js
+++ Routes/fileManagement/upsertDir.js
@@ -1,12 +1,31 @@
 const fs=require('fs')
+const os=require('os');
+const path=require('path');
 const upsertDir=async (dirpath)=>{
-    if(fs.existsSync(dirpath)){
-        console.log("in upsertDir : ",dirpath," already exist")
+    try{
+        try{
+            let basedirpath=path.join(os.tmpdir(),'SaraswatiCodingClub');
+            if(fs.existsSync(basedirpath)){
+                console.log("already exists basedirpath : ",basedirpath);
+            }else{
+                fs.mkdirSync(basedirpath);
+                console.log("created basedirpath : ",basedirpath)
+            }
+        }catch(err){
+            console.log("error while basedirpath creation",err)
+        }
+        if(fs.existsSync(dirpath)){
+            console.log("in upsertDir : ",dirpath," already exist")
+            return dirpath;
+        }
+        fs.mkdirSync(dirpath);
+        console.log("in upsertDir created dir : ",dirpath);
+        return dirpath;
+    }catch(err){
+        console.log(err);
         return dirpath;
     }
-    fs.mkdirSync(dirpath);
-    console.log("in upsertDir created dir : ",dirpath);
-    return dirpath;
+    
 }
 module.exports={
     upsertDir:upsertDir
